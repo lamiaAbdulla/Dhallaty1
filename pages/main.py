e7888c713119e1b52bd0e9a4479b863f2766eda2
lamiaAbdulla <134142448+lamiaAbdulla@users.noreply.github.com>
2024-04-13 16:11:43 +0300

    Update main.py

--- pages/main.py
+++ pages/main.py
@@ -7,64 +7,103 @@ import time
 import pandas as pd
 from PIL import Image
 from dotenv import load_dotenv
+import boto3
 
+
+
+def configure():
+    load_dotenv()
 
 
 openai.api_key = os.getenv("api_secret")
-
-load_dotenv()
+#for aws
+SECRET_ACCESS_KEY = os.getenv("SECRET_ACCESS_KEY")
+KEY_ID = os.getenv("KEY_ID")
+configure()
 
 responses_df = pd.read_csv("secure_responses.csv", encoding='utf-8') if "secure_responses.csv" in os.listdir() else pd.DataFrame()
 
 st.set_page_config(page_title="Dhallaty", page_icon=":mag_right:", layout="wide")
 
+#-------------------------------start images code block--------------------------------------------
 
-# User input page
-st.title("مساعدك لضالتك")
+#credentials for s3 aws
+REGION_NAME = 'us-east-1'
+BUCKET_NAME = 'dhallaty-imagess'
 
 
-def join_and_submit(text_input, uploaded_file):
+s3 = boto3.client( #S3 client
+    's3',
+    aws_access_key_id=KEY_ID,
+    aws_secret_access_key=SECRET_ACCESS_KEY,
+    region_name=REGION_NAME
+)
 
+#upload images to S3 bucket
+def upload_to_s3(file, bucket_name, key):
+    s3.upload_fileobj(file, bucket_name, key)
+    return st.success("File uploaded successfully!")
+
+
+def get_public_image_url(key):
+    public_url = f"https://{BUCKET_NAME}.s3.{REGION_NAME}.amazonaws.com/{key}"
+    return public_url
+
+
+def uploaded_image():
+    uploaded_file = st.file_uploader("أرفقي صورة للغرض إن وجد (سواءً صورة من الإنترنت أو من صورك)")
+    if uploaded_file is not None:
+        st.image(uploaded_file, caption="Uploaded Image", use_column_width=True)
+
+        key = uploaded_file.name  
+        upload_to_s3(uploaded_file, BUCKET_NAME, key)
+
+        image_url = get_public_image_url(key)
+        return image_url
+
+
+def join_and_submit(text_input, image_url = None):
   data = {}
+
   data["text"] = text_input
-  if uploaded_file is not None:
-    bytes_data = uploaded_file.read()
-    image = Image.open(io.BytesIO(bytes_data))  # Convert bytes to image
-    data["image"] = bytes_data  # Store image data with key "image"
+  if image_url is not None:
+    data["image"] = image_url 
+
   return data
+#----------------------------------------------end images code block-------------------------------------------------------
+
+#---------------------------------------start of openai.chat.completions.create--------------------------------------------
+st.title("مساعدك لضالتك")
+st.write(":violet[نرجو كتابة الوصف مع مراعاة (اللون / المكان /العلامة التجارية) ]")
 
 user_input = st.text_area("اوصف ضالتك هنا")
-uploaded_file = st.file_uploader("ارفقي صورة للغرض إن وجد")
+image_url = uploaded_image()
 
-#التيمب ليست هنا بس حطيتها عشان بعد كل  ريكويست يطلع شكل تحليل جي بي تي له في نفس الصفحة
-temp_list = []
-if 'temp_list' not in st.session_state:
-      st.session_state['temp_list'] = temp_list
+
 
 if st.button("ارسلي الوصف"):
-    joined_data = join_and_submit(user_input, uploaded_file)
+
     response = openai.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system", "content": f"Please analyze the following description of a lost item and ONLY ANSWER with this exact format. type, brand, color, place. stick with the format,FOUR sections only.you can answer with a sorry message if you did not understand the input. NOTE: here is some words that you might face with their definitions (1- حلق = Earrings 2- شنطة = bag 3- سلسال = necklace 4- سواره = bracelet):\n{user_input}"},
+            {"role": "system", "content": f"Please analyze the following description of a lost item and ONLY ANSWER with this exact format. type, brand, color, place. stick with the format,FOUR sections only (NO MATTER WHAT). you can answer with a sorry message if you did not understand the input. NOTE: here is some words that you might face with their definitions (1- حلق = Earrings 2- شنطة = bag 3- سلسال = necklace 4- سواره = bracelet):\n{user_input}"},
  ])
 
     analysis = response.choices[0].message.content
 
+    joined_data = join_and_submit(user_input, image_url)
     if len(analysis.split(",")) >= 4:
-     new_row = pd.DataFrame({"description": [user_input], "analysis": [analysis],"Photo": joined_data.get("image", None)})
+     image_url = joined_data.get("image", None)
+     new_row = pd.DataFrame({"description": [user_input], "analysis": [analysis],"image": [image_url]})
      responses_df = pd.concat([responses_df, new_row], ignore_index=True)
 
-    #Save to CSV
+
      responses_df.to_csv("secure_responses.csv", index=False)
 
      st.success("تم إرسال وصفك للقسم الأمني.")
     else:
        st.error("حدث فشل في إرسال الطلب، الرجاء التأكد من ارسال وصف واضح ") 
 
-    #هنا يعرض التيمب ليست (فقط للتأكد انها مضبوطه))
-    #test_list = analysis.split(",")
-    #st.write(test_list)
 
 #  -----------------------------------------end of openai.chat.completions.create --------------------------------------------------
 
@@ -76,7 +115,7 @@ def edit_df1():
     try:
 
       description = row.description
-      Photo = row.Photo
+      image = row.image
       analysis = str(row.analysis)  #ensure that it is a string
       analysis_list = analysis.split(",")
 
@@ -86,7 +125,7 @@ def edit_df1():
         items_with_description["items"][item_key] = {
           "analysis": analysis_list[:4],
           "description": description,
-          "Photo": Photo
+          "image": image
         }
     except (KeyError, AttributeError) as e:
 
@@ -97,4 +136,3 @@ def edit_df1():
   
 lost_items = edit_df1() 
 st.session_state["lost_items"] = lost_items
-#st.write(lost_items.items()) # عشان اتأكد من القيم فقط
